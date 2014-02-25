d22aa763699648bd9ee4ad383d57cf88fff67664
Alex0216 <stonge.alex@gmail.com>
2014-02-25 21:40:05 +0100

    Initial commit!

---
+++ src/View/mainView.java
@@ -0,0 +1,17 @@
+package View;
+
+import javax.swing.*;
+
+/**
+ * Created by Alexandre on 25/02/14.
+ * This is the Main windows of the application. It contain an
+ * ActivityBankPanel at the left, one or more DayPanel in the center with a
+ * button to add more DayPanel if needed.
+ * Project: ${PROJECT_NAME}.${PACKAGE_NAME}.${FILE_NAME}
+ */
+public class MainView extends JFrame {
+
+    public MainView() {
+
+    }
+}
